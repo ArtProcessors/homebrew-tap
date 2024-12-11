38a68a9204f6383d82135153a0b2a20d2fe6b40a
Vlas Voloshin <argentumko@gmail.com>
2024-12-11 11:12:07 +1100

    chore: APM-2623 updated poeditor formula

--- Formula/poeditor.rb
+++ Formula/poeditor.rb
@@ -4,9 +4,9 @@ require_relative "lib/private_strategy"
 class Poeditor < Formula
   desc "POEditor CLI tool"
   homepage "https://github.com/ArtProcessors/ap-poeditor-tool-macos"
-  url "https://github.com/ArtProcessors/ap-poeditor-tool-macos/releases/download/2.11.0/poeditor.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "2.11.0"
-  sha256 "1a657cd25d026a64f6bc304d0d814e1ffbe3670cf7ca3d44b8a7965851b19f4e"
+  url "https://github.com/ArtProcessors/ap-poeditor-tool-macos/releases/download/2.11.1/poeditor.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "2.11.1"
+  sha256 "ee7f3658277a9d56f63cb476495d9b4eac7260d7feed78834c8775dd9786c576"
 
   def install
     bin.install "poeditor"
