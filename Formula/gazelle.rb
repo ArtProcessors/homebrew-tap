fb0a362da7a9bf4ac6025c3c67b133e268ff528a
Vlas Voloshin <argentumko@gmail.com>
2025-01-06 20:00:32 +1100

    chore: updated gazelle formula

--- Formula/gazelle.rb
+++ Formula/gazelle.rb
@@ -4,9 +4,9 @@ require_relative "lib/private_strategy"
 class Gazelle < Formula
   desc "Gazelle Schema code generator and validator"
   homepage "https://github.com/ArtProcessors/ap-gazelle-schema-mobile"
-  url "https://github.com/ArtProcessors/ap-gazelle-schema-mobile/releases/download/2.6.1/gazelle.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "2.6.1"
-  sha256 "0edf94ee4f7158a45e80fcfb29bd2e530d53afa8311d7cfbc1d55b3b5758bd4a"
+  url "https://github.com/ArtProcessors/ap-gazelle-schema-mobile/releases/download/2.7.0/gazelle.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "2.7.0"
+  sha256 "1d5a637027761a2639e666adb803ffb1ff6d39b84f1025d2b81b55f39bbf45fc"
 
   def install
     bin.install "gazelle"
